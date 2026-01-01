a34e2bd888e8787dac8d04feb480630ae819a10c
johnmcgettrick <johnmcgettrick@gmail.com>
2026-01-01 21:37:17 +0000

    Add admin section for locations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import MapPage from "./MapPage";
 import Admin from "./Admin";
+import AdminLocations from "./AdminLocations";
+
 import "./App.css";
 
 export default function App() {
@@ -9,12 +11,14 @@ export default function App() {
     <Router>
       <nav className="top-nav">
         <Link to="/">Map</Link>
-        <Link to="/admin">Admin</Link>
+        <Link to="/admin">Runs Admin</Link>
+        <Link to="/admin-locations">Locations Admin</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<MapPage />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/admin-locations" element={<AdminLocations />} />
       </Routes>
     </Router>
   );
